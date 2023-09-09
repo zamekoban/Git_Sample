1ff8d795f90371a6d9763a22f719a0a8832b7310
zamekoban <unison3667b@outlook.jp>
2023-09-09 22:44:10 +0900

    add a tan(x)

--- Python_test/test.py
+++ Python_test/test.py
@@ -12,10 +12,12 @@ xmin, xmax = -np.pi, np.pi
 x = np.arange(xmin, xmax, 0.1)
 y_sin = np.sin(x)
 y_cos = np.cos(x)
+y_tan = np.tan(x)
 plt.plot(x, y_sin)
 plt.plot(x, y_cos)
+plt.plot(x, y_tan)
 plt.hlines([-1, 1], xmin, xmax, linestyles="dashed")
-plt.title(r"sin(x) and cos(x)")
+plt.title(r"sin(x) and cos(x) and tan(x)")
 plt.xlim(xmin, xmax)
 plt.ylim(-1.3, 1.3)
 
